a49bedec0643c7fa28892b0633e57b6df03415dd
Fityulane <208073556+Fityulane@users.noreply.github.com>
2025-11-24 22:58:12 +0100

    show users

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { checkAdmin } = require('../modules/auth');
+const { query } = require("../modules/db");
+const { checkAdmin } = require("../modules/auth");
 
-router.get('/', checkAdmin, function(req, res, next) {
-  res.render('admin', { title: 'Admin oldal', user: req.user });
+router.get("/", checkAdmin, async function (req, res, next) {
+  try {
+    const users = await query("SELECT * FROM users");
+    res.render("admin", { title: "Admin oldal", user: req.user, users: users });
+  } catch (error) {
+    req.flash("error", "Hiba történt a felhasználók lekérése során!");
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
