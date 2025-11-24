da16d823d85276e883472186ab00b852e7c09914
szbgreg <74787170+szbgreg@users.noreply.github.com>
2025-11-24 16:54:36 +0100

    feat: list cakes from database

--- routes/database.js
+++ routes/database.js
@@ -1,8 +1,45 @@
-var express = require('express');
+var express = require("express");
+const { query } = require("../modules/db");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('Ez lesz itt az adatbázis menüpont.');
+router.get("/", async function (req, res, next) {
+  try {
+    // 1. Sütik lekérdezése
+    const sutik = await query(`SELECT * FROM suti ORDER BY id`);
+
+    for (let suti of sutik) {
+      // Tartalmak
+      suti.tartalmak = await query(
+        `SELECT * FROM tartalom WHERE sutiid = '${suti.id}'`
+      );
+
+      // Árak
+      suti.arak = await query(
+        `SELECT * FROM ar WHERE sutiid = '${suti.id}' ORDER BY ertek`
+      );
+    }
+
+    const mentesMap = {
+      G: "Gluténmentes",
+      L: "Laktózmentes",
+      HC: "Hozzáadott cukor nélkül",
+      Te: "Tejmentes",
+      To: "Tojásmentes",
+      É: "Édesítőszerrel készült",
+    };
+
+    res.render("database", {
+      title: "Süti adatbázis",
+      sutik: sutik,
+      mentesMap: mentesMap,
+
+    });
+
+  } catch (error) {
+    console.error("Hiba a sütik lekérdezésekor:", error);
+    req.flash("error", "Hiba történt a sütik lekérdezésekor!");
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
